794895c5aa4b1fc69169790e2572a25532ca8108
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-12-30 21:16:55 +0700

    edit cors

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::prefix('api')->middleware('api.key')->group(function () {
-//     Route::get('/properties', [WebScrapperController::class, 'index']);
-//     Route::get('/properties/{region}', [WebScrapperController::class, 'index']);
-//     Route::get('/properties/{region}/{city}', [WebScrapperController::class, 'index']);
-//     Route::get('/properties/{region}/{city}/{district}', [WebScrapperController::class, 'index']);
-//     Route::get('/property/{slug}', [WebScrapperController::class, 'show']);
-// });
-
-Route::middleware('api')->group(function () {
+Route::prefix('api')->middleware('api.key')->group(function () {
     Route::get('/properties', [WebScrapperController::class, 'index']);
     Route::get('/properties/{region}', [WebScrapperController::class, 'index']);
     Route::get('/properties/{region}/{city}', [WebScrapperController::class, 'index']);
     Route::get('/properties/{region}/{city}/{district}', [WebScrapperController::class, 'index']);
     Route::get('/property/{slug}', [WebScrapperController::class, 'show']);
 });
-
-
-
